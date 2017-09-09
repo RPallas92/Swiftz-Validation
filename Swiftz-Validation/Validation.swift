a13ff49d5412f0c2b6c04cbadf1cd21779dc25d9
Ricardo Pallás <rpallas92@gmail.com>
2017-09-09 18:59:44 +0200

    Applicative and applicative ops

--- Swiftz-Validation/Validation.swift
+++ Swiftz-Validation/Validation.swift
@@ -31,11 +31,12 @@ public enum Validation<L,R> {
     
 }
 
-extension Validation {  //Functor
+extension Validation /*: Functor*/ {
+    public typealias A = R
     public typealias B = Any
     public typealias FB = Validation<L,B>
     
-    public func fmap<B>(_ f : @escaping (R) -> B) -> Validation<L,B> {
+    public func fmap<B>(_ f : @escaping (A) -> B) -> Validation<L,B> {
         switch self {
         case .Failure(let a):
             return Validation<L,B>.Failure(a)
@@ -45,8 +46,43 @@ extension Validation {  //Functor
     }
 }
 
-extension Validation { //Pointed
+extension Validation /*: Pointed*/ {
     public static func pure(_ x : R) -> Validation<L,R> {
         return Validation.Success(x)
+    }
+}
+
+
+extension Validation /*: Applicative*/ {
+    public typealias FAB = Validation<L, (A) -> B>
+    
+    public func ap<B>(_ f : Validation<L,(A) -> B>) -> Validation<L,B> {
+        switch self {
+        case .Success(let b):
+            return f.fmap{ $0(b) }
+        case .Failure(let l):
+            return Validation<L,B>.Failure(l)
+
+        }
+    }
+}
+
+extension Validation /*: ApplicativeOps*/ {
+    public typealias C = Any
+    public typealias FC = Validation<L,C>
+
+    
+    public static func liftA<B>(_ f : @escaping (A) -> B) -> (Validation<L,A>) -> Validation<L,B> {
+        return { (a : Validation<L,A>) -> Validation<L,B> in
+            return a.ap(Validation<L, (A) -> B>.pure(f))
+        }
+    }
+
+    public static func liftA2<B>(_ f : @escaping (A) -> (B) -> C) -> (Validation<L,A>) -> (Validation<L,B>) -> Validation<L,C> {
+        return { (a : Validation<L,A>) -> (Validation<L,B>) -> Validation<L,C> in
+            { (b:Validation<L,B>) -> Validation<L,C> in
+                return b.ap(a.fmap(f))
+            }
+        }
     }
 }
